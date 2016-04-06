a0a903c6e7921d7863c8a8065e898b496581bc65
bmagnusson <magnusson.brent@gmail.com>
2016-04-06 10:39:27 -0700

    this is getting annoying

--- sgsc.py
+++ sgsc.py
@@ -61,6 +61,9 @@ def get_and_save_han_dynamo(customer_id):
     # os.system("env")
     print("sgsc line 55")
     print(type(boto3))
+    client = boto3.client('dynamodb', aws_access_key_id=key,
+                            aws_secret_access_key=secret)
+    print(type(client))
     dynamo = boto3.resource('dynamodb', aws_access_key_id=key,
                             aws_secret_access_key=secret)
     print("sgsc line 57")
